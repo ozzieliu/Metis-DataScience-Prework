2f004e2f3615afab11f34ba5b3fba8ef8469c1f4
Ozzie Liu <ozzie.liu@rutgers.edu>
2015-12-20 16:08:58 -0500

    Uploading Markov code along with 3 text to sample from. Version 1 will parse the text in all lowercase and strip punctuation to generate new text of desired length.

--- python/markov.py
+++ python/markov.py
@@ -1,15 +1,92 @@
 #!/usr/bin/env python
+# encoding: utf-8
 
 # Write a Markov text generator, [markov.py](python/markov.py). Your program should be called from the command line with two arguments: the name of a file containing text to read, and the number of words to generate. For example, if `chains.txt` contains the short story by Frigyes Karinthy, we could run:
-
 # ```bash
 # ./markov.py chains.txt 40
 # ```
 
-# A possible output would be:
+## Available text for Markov generator:
+## - emma.txt (Jane Austen's Emma)
+## - eric.txt (Song "Eric, half a bee")
+## - delcaration.txt (The Delcaration of Independence)
 
-# > show himself once more than the universe and what I often catch myself playing our well-connected game went on. Our friend was absolutely correct: nobody from the group needed this way. We never been as the Earth has the network of eternity.
+## Currrently, we are stripping all words from the original text of punctuation and converting it all to lowercase.
+## The output would be a string of words of the desired length
+## While the desired suffix length is currently set to 4. (1 word + 4 subsequent words). This can be changed in the main function.
 
-# There are design choices to make; feel free to experiment and shape the program as you see fit. Jeff Atwood's [Markov and You](http://blog.codinghorror.com/markov-and-you/) is a fun place to get started learning about what you're trying to make.
+## Next steps would be to factor in capitalized words and punctuation.
 
-> REPLACE THIS TEXT WITH YOUR PROGRAM
+### Code for creating a histogram of each word in the text:
+import string
+import random
+import sys
+
+### Code to make a dictionary of subsequent words
+### Takes input of file name and prefix length
+def create_word_dict(filename, prefix_length):
+    word_list = []
+    word_dict = {}
+
+    ## Open the file...
+    with open(filename, "r") as text:
+        ## And read each line in text file and store it in an ordered list of all words
+        for line in text:
+            for word in line.split():
+                word = word.strip(string.punctuation + string.whitespace).lower()
+                word_list.append(word)
+
+    ## Go through the word list word and make a dictionary of each words' subsequent words
+    for index, word in enumerate(word_list):
+        ## For each word before the last few
+        if index < len(word_list)-prefix_length+1:
+            if word not in word_dict:
+                word_dict[word] = [word_list[index+1:index+prefix_length]]
+            elif word in word_dict:
+                word_dict[word].extend([word_list[index+1:index+prefix_length]])
+
+        ## For the last few words
+        else:
+            ## Add an empty string for the last word if it's not in the dict yet
+            if index == len(word_list)-1:
+                if word not in word_dict:
+                    word_dict[word] = ['']
+            ## For the few words before the last one
+            else:
+                if word not in word_dict:
+                    word_dict[word] = [word_list[index+1:]]
+                elif word in word_dict:
+                    word_dict[word].extend([word_list[index+1:]])
+
+    #print word_dict
+    return word_dict
+
+def markov_generator(dictionary, length):
+    ## First word:
+    text = random.sample(dictionary.keys(), 1)
+    #print text
+
+
+    ## Iterate through subsequent n words:
+    while len(text) < length:
+        ## find the new word to add:
+        new_word = random.sample(dictionary[text[-1]], 1)[0]
+        #print new_word
+        ## Add that list to the text
+        text.extend(new_word)
+
+    ## Cut off additional words if exceeding length limit
+    if len(text) > length:
+        text = text[:length]
+
+    print len(text)
+    return " ".join(text)
+
+## Main section
+def main(argv):
+    my_dict = create_word_dict(sys.argv[1], 5)
+    #print type(sys.argv[1]), type(sys.argv[2])
+    print markov_generator(my_dict, int(sys.argv[2]))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
